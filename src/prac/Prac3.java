136aa283e7458d713e4ac422112e97793bac0db6
Esc-04 <escho94@naver.com>
2026-03-28 15:27:41 +0900

    [Feat] Practice 3

--- src/prac/Prac3.java
+++ src/prac/Prac3.java
@@ -1,10 +1,21 @@
 package prac;
 
+import java.util.Scanner;
+
 public class Prac3 {
     public static void main(String[] args) {
         //앞에 공백 없이 오름차순으로 별 출력하기
 
         //코드 작성하기
+        Scanner in=new Scanner(System.in);
+        System.out.print("출력 할 줄 개수 입력 : ");
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
 
