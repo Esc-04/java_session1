1e52a4538d17c715c1972693d16b015fe1680851
Esc-04 <escho94@naver.com>
2026-03-28 15:32:13 +0900

    [Feat] Practice 4

--- src/prac/Prac4.java
+++ src/prac/Prac4.java
@@ -1,11 +1,23 @@
 package prac;
 
+import java.util.Scanner;
+
 public class Prac4 {
     public static void main(String[] args) {
         //앞에 공백 있이 오름차순으로 별 출력하기
 
         //코드 작성하기
-
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int i=0;i<n-row;i++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<row;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
 
