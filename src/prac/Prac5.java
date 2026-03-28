f565b821a9deb1c0e2b37dfb987aa0aee05a2441
Esc-04 <escho94@naver.com>
2026-03-28 15:48:09 +0900

    [Feat] Practice5

--- src/prac/Prac5.java
+++ src/prac/Prac5.java
@@ -1,11 +1,27 @@
 package prac;
 
+import java.util.Scanner;
+
 public class Prac5 {
     public static void main(String[] args) {
         //정수 두 개를 입력 받고, 해당 정수들 사이에 있는 값을 출력하자.
         //첫번째 숫자가 두번째 숫자보다 클 때 적용할 두 숫자를 서로 교환하는 조건문도 작성하자.
 
         //코드 작성하기
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        if (n>m){
+            int tmp=n;
+            n=m;
+            m=tmp;
+        }
+        for(int i=n;i<=m;i++){
+            System.out.print(i);
+            if(i!=m){
+                System.out.print(", ");
+            }
+        }
 
 
 
