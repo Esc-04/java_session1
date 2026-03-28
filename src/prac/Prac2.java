cb44f0b6d5b9c1acdc45442c7ab73a1e23e885bb
Esc-04 <escho94@naver.com>
2026-03-28 15:17:05 +0900

    [Feat] Practice2

--- src/prac/Prac2.java
+++ src/prac/Prac2.java
@@ -1,11 +1,19 @@
 package prac;
-
+import java.util.Scanner;
 public class Prac2 {
     public static void main(String[] args) {
         //하나의 정수를 입력받고, 해당 정수가 홀수인지 짝수인지 판별하기
         //반복문으로 계속 입력받고 판별할 수 있게 하고, 정수 입력에 있어 -1을 입력 시 프로그램 종료가 되도록 하기
 
         //코드 작성하기
+        Scanner in=new Scanner(System.in);
+        int n;
+        System.out.print("정수 입력 받기 : ");
+        while((n=in.nextInt())!=-1){
+            System.out.printf("입력한 숫자 %d는 %s입니다.\n",n,(n%2==0)? "짝수":"홀수");
+            System.out.print("정수 입력 받기 : ");
+        }
+        System.out.println("프로그램 종료");
 
 
 
