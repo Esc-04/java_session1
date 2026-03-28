d9ea21c26aadc0baa17f4a17dcfc7651d5427fdc
Esc-04 <escho94@naver.com>
2026-03-28 15:12:25 +0900

    [Feat] Practice1

--- src/prac/Prac1.java
+++ src/prac/Prac1.java
@@ -1,14 +1,15 @@
 package prac;
-
+import java.util.Scanner;
 public class Prac1 {
     public static void main(String[] args) {
         //이름과 나이를 입력받고 출력하기
-
-        //코드 작성하기
-
-
-
-
+        Scanner in=new Scanner(System.in);
+        int age; String name;
+        System.out.print("당신의 이름을 입력 : ");
+        name=in.next();
+        System.out.print("당신의 나의를 입력 : ");
+        age=in.nextInt();
+        System.out.printf("당신의 이름은 %s이고, 나이는 %d살 입니다.",name,age);
 
 
 
